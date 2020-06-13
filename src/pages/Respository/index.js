1685dee5c043ce862290086995949e3fcdba518d
Jairo <jairopereira_dias@hotmail.com>
2020-06-13 06:10:45 -0300

    listando todas as issues dentro de um repositório do github

--- src/pages/Respository/index.js
+++ src/pages/Respository/index.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-
-import { Loading } from './styles';
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList} from './styles';
 
 export default class Respository extends Component{
 
@@ -50,7 +51,29 @@ export default class Respository extends Component{
       return <Loading>Carregando...</Loading>
     }
     return (
-      <h1>Respository</h1>
+      <Container>
+        <Owner>
+          <Link to="/"> Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
+          <h1>{repository.name}</h1>
+          <p>{repository.name}</p>
+        </Owner>
+
+        <IssueList>
+          {issues.map( issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login}/>
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
     )
   }
 
